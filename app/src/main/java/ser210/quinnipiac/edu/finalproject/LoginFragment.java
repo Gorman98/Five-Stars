0b46e1d891571416b8259d2c7280d21d7b8172db
Gorman98 <31937140+Gorman98@users.noreply.github.com>
2018-04-16 19:31:18 -0400

    fixed add issue

--- app/src/main/java/ser210/quinnipiac/edu/finalproject/LoginFragment.java
+++ app/src/main/java/ser210/quinnipiac/edu/finalproject/LoginFragment.java
@@ -2,7 +2,7 @@ package ser210.quinnipiac.edu.finalproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
