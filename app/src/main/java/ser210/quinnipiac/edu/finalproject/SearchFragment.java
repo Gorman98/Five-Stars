352bc6fc8fc98aaab3de1f76bd80160776749e9c
DizzleDuff <31937154+DizzleDuff@users.noreply.github.com>
2018-04-17 12:56:05 -0400

    adding search activity

---
+++ app/src/main/java/ser210/quinnipiac/edu/finalproject/SearchFragment.java
@@ -0,0 +1,23 @@
+package ser210.quinnipiac.edu.finalproject;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+
+public class SearchFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
