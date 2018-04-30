19e14d7ea79575558c2fbf5720537fac368e6af2
Gorman98 <31937140+Gorman98@users.noreply.github.com>
2018-04-30 13:37:23 -0400

    api work

--- app/src/main/java/ser210/quinnipiac/edu/finalproject/Worker.java
+++ app/src/main/java/ser210/quinnipiac/edu/finalproject/Worker.java
@@ -2,9 +2,15 @@ package ser210.quinnipiac.edu.finalproject;
 
 import android.app.ListFragment;
 import android.os.AsyncTask;
+import android.provider.DocumentsContract;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,6 +22,13 @@ import java.net.MalformedURLException;
 import java.net.PasswordAuthentication;
 import java.net.URL;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+
 /**
  * Created by Kyle on 3/9/2018.
  */
@@ -23,9 +36,12 @@ import java.net.URL;
 public class Worker extends AsyncTask<String, String, String> {
     private SearchFragment searchFragment;
     private String json;
+    Boolean jsonCheck;
+    private Document doc;
 
-    public Worker(SearchFragment search) throws JSONException {
+    public Worker(SearchFragment search, Boolean jsonCheck) throws JSONException {
         this.searchFragment = search;
+        this.jsonCheck = jsonCheck;
         json = null;
     }
 
@@ -38,41 +54,55 @@ public class Worker extends AsyncTask<String, String, String> {
     protected String doInBackground(String... strings) {
         HttpURLConnection connection = null;
         BufferedReader bufferedReader = null;
-        try {
-            URL url = new URL(strings[0]);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
-
-            InputStream inputStream = connection.getInputStream();
-
-            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            StringBuffer stringBuffer = new StringBuffer();
-            String line = "";
-
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuffer.append(line);
-            }
+        if (jsonCheck) {
             try {
-                json = stringBuffer.toString();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return stringBuffer.toString();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-            try {
-                if (bufferedReader != null) {
-                    bufferedReader.close();
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuffer stringBuffer = new StringBuffer();
+                String line = "";
+                if(jsonCheck)
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuffer.append(line);
                 }
+                try {
+                    json = stringBuffer.toString();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return stringBuffer.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (bufferedReader != null) {
+                        bufferedReader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
+        } else {
+            URL url = null;
+            try {
+                url = new URL(strings[0]);
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                doc = db.parse(new InputSource(url.openStream()));
+                doc.getDocumentElement().normalize();
+            } catch (Exception e) {
+                System.out.println("XML Pasing Excpetion = " + e);
+            }
+
         }
         return null;
     }
@@ -80,13 +110,16 @@ public class Worker extends AsyncTask<String, String, String> {
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
-        try {
-            JSONObject jsonObject = new JSONObject(json);
-            searchFragment.result(jsonObject);
-        } catch (JSONException e) {
-            e.printStackTrace();
+        if(jsonCheck) {
+            try {
+                JSONObject jsonObject = new JSONObject(json);
+                searchFragment.jsonResult(jsonObject);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
-
-
+        else {
+            searchFragment.xmlResult(doc);
+        }
     }
 }
