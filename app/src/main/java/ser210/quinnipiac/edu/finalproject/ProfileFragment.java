8f5aa5de42fdbd5c5bd77f78e6fe41961d54d640
DizzleDuff <31937154+DizzleDuff@users.noreply.github.com>
2018-05-01 10:33:03 -0400

    upload profile image

--- app/src/main/java/ser210/quinnipiac/edu/finalproject/ProfileFragment.java
+++ app/src/main/java/ser210/quinnipiac/edu/finalproject/ProfileFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,13 +13,19 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
+import static android.app.Activity.RESULT_OK;
+
 
 public class ProfileFragment extends Fragment {
+
+    private static final int RESULT_LOAD_IMAGE = 1;
+    ImageView profile;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,6 +33,27 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         final View iview = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        TextView username = (TextView) iview.findViewById(R.id.usernameProfile);
+        username.setText(MainActivity.userLoggedIn);
+
+        profile = (ImageView) iview.findViewById(R.id.profilePic);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+            }
+        });
+
         return iview;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode ==  RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
+            Uri selectedImage = data.getData();
+            profile.setImageURI(selectedImage);
+        }
     }
 }
