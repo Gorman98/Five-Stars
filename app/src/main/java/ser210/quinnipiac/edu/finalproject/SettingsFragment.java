885e0479107e836115bd99d94f37a4f33caf0c13
Gorman98 <31937140+Gorman98@users.noreply.github.com>
2018-05-02 15:39:16 -0400

    you know coding stuff

--- app/src/main/java/ser210/quinnipiac/edu/finalproject/SettingsFragment.java
+++ app/src/main/java/ser210/quinnipiac/edu/finalproject/SettingsFragment.java
@@ -11,14 +11,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class SettingsFragment extends Fragment {
     private ConstraintLayout settingsFrag;
-
-    public SettingsFragment() {
-    }
+    private FirebaseDatabase database;
+    private DatabaseReference users;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,6 +33,7 @@ public class SettingsFragment extends Fragment {
         Button whiteButton = (Button) v.findViewById(R.id.whiteButton);
         Button blueButton = (Button) v.findViewById(R.id.blueButton);
         Button greenButton = (Button) v.findViewById(R.id.greenButton);
+        Button deleteButton = (Button) v.findViewById(R.id.deleteButton);
 
         whiteButton.setOnClickListener(new View.OnClickListener()
         {
@@ -66,6 +69,19 @@ public class SettingsFragment extends Fragment {
                 //run function that checks sql for username and password
                 MainActivity.setColorVal(2);
                 settingsFrag.setBackgroundColor(getResources().getColor(MainActivity.color));
+            }
+        });
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view = v;
+                view.setEnabled(false);
+
+                database = FirebaseDatabase.getInstance();
+                users = database.getReference("Users");
+                users.child(MainActivity.userLoggedIn).removeValue();
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
             }
         });
         return v;
