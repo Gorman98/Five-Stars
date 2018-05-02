e2947bc92a670b5f231e8d60c3af7c67debfaa95
Gorman98 <31937140+Gorman98@users.noreply.github.com>
2018-05-02 16:37:47 -0400

    you wont

--- app/src/main/java/ser210/quinnipiac/edu/finalproject/ProfileFragment.java
+++ app/src/main/java/ser210/quinnipiac/edu/finalproject/ProfileFragment.java
@@ -18,8 +18,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
