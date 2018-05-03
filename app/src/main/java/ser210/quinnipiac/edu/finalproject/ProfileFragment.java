652e0546c364381c156f3b23cd4b1ca3f35fab23
Ryan Hanlon <rjhanlon@quinnipiac.edu>
2018-05-02 23:47:52 -0400

    Make a cool adapter and other cool stuff that allows you to see all those great reviews :)

--- app/src/main/java/ser210/quinnipiac/edu/finalproject/ProfileFragment.java
+++ app/src/main/java/ser210/quinnipiac/edu/finalproject/ProfileFragment.java
@@ -115,6 +115,10 @@ public class ProfileFragment extends Fragment {
                 for(DataSnapshot dsp : dataSnapshot.child(user).child("Review").getChildren()) {
                     System.out.println("We cooking meow");
                     Log.d("Review ", String.valueOf(dsp.getKey()));
+                    System.out.println("Title: " + dsp.getValue().toString());
+                    System.out.println("Review: " + dsp.child("Review Statement").getValue().toString());
+                    System.out.println("Rating: " + dsp.child("Rating").getValue().toString());
+
                     System.out.println(reviewList.size());
                     reviewList.add(new Review(dsp.getKey(), dsp.child("Review Statement").getValue().toString(),dsp.child("Rating").getValue().toString()));
                     reviewAdapter = new ReviewAdapter(getContext(), reviewList);
