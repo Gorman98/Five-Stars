5c5a8ac48d04c306d722619c720eabe8d2024cf1
Gorman98 <31937140+Gorman98@users.noreply.github.com>
2018-05-03 00:14:52 -0400

    and done now

--- app/src/androidTest/java/ser210/quinnipiac/edu/finalproject/AppUITest.java
+++ app/src/androidTest/java/ser210/quinnipiac/edu/finalproject/AppUITest.java
@@ -6,6 +6,7 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
